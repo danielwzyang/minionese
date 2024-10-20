49951a3ddbfb052c9e925bd52d9fe5b1868c1ecd
Daniel Yang <danielwzyang@gmail.com>
2024-10-20 19:44:53 -0400

    updated readfile test

--- test/ReadFile.java
+++ test/ReadFile.java
@@ -2,6 +2,10 @@ package test;
 import java.io.File;
 import java.util.Scanner;
 
+import interpreter.BoolVal;
+import interpreter.Environment;
+import interpreter.Interpreter;
+import interpreter.NumberVal;
 import parser.Parser;
 import parser.Program;
 
@@ -9,7 +13,7 @@ public class ReadFile {
     public static void main(String[] args) {
         try {
             if (args.length < 1) {
-                System.err.println("No file name provided.");
+                System.err.println("No file name provided. Example call: java test/ReadFile.java test/test.minion");
                 System.exit(0);
             }
 
@@ -20,9 +24,15 @@ public class ReadFile {
             src += reader.next();
             reader.close();
 
+            // declare testing variables
+            Environment globalEnvironment = new Environment();
+            globalEnvironment.declareVariable("x", new NumberVal(3), false);
+            globalEnvironment.declareVariable("y", new BoolVal(), false);
+            
             Parser parser = new Parser();
             Program program = parser.makeAST(src);
-            System.out.println(program);
+            Interpreter interpreter = new Interpreter();
+            System.out.println(interpreter.evaluate(program, globalEnvironment));
         } catch (Exception error) {
             System.err.println(error);
             System.exit(0);
